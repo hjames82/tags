a23be6487e2d5652eafae94560f6ac3a45d19115
hjames82 <james.helms80@gmail.com>
2023-12-14 09:39:31 -0600

    Add files via upload

---
+++ composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.3',
+        'version' => '1.0.3.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0ff0f2ee4f485ef3a1939a4096ecab0a8e31e1ca',
+        'name' => '__root__',
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.3',
+            'version' => '1.0.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0ff0f2ee4f485ef3a1939a4096ecab0a8e31e1ca',
+            'dev_requirement' => false,
+        ),
+        'hivepress/hivepress-updates' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../hivepress/hivepress-updates',
+            'aliases' => array(),
+            'reference' => '1fa0b5d14d26f8752f46cae6545f047a837c845e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
